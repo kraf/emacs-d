dcda1188c509c86007fce6756c154c01f1c6bacc
kraf <filip.krasnianski@gmail.com>
2015-10-25 08:38:06 +0100

    add expand-region, cleanup

--- customizations/evil-mode.el
+++ customizations/evil-mode.el
@@ -14,9 +14,8 @@
 (define-key evil-insert-state-map "\C-e" 'move-end-of-line)
 (define-key evil-normal-state-map "\C-k" 'paredit-kill)
 (define-key evil-insert-state-map "\C-k" 'paredit-kill)
-(define-key evil-normal-state-map ",c\ " 'evilnc-comment-or-uncomment-lines)
-(define-key evil-visual-state-map ",c\ " 'evilnc-comment-or-uncomment-lines)
 (define-key evil-normal-state-map "K" 'evil-previous-line)
+(define-key evil-normal-state-map ",e" 'er/expand-region)
 
 (add-hook 'paredit-mode-hook
           (lambda ()
